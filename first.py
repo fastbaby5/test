d02530cac91f65ed01dd864589125033fa124f32
baha salameh <baha@MacBookAir.home>
2025-07-25 05:36:48 +0300

    this is my first commit

---
+++ first.py
@@ -0,0 +1,59 @@
+print ("hello, world!")
+
+# how to deal with str and value to print.
+#name = input("name: ")
+#print (f"heloooooooooooooo {name}")
+
+
+
+
+
+# conditions
+#n =  int(input ("number: "))
+#if n > 0:
+#    print ("the number is greater 0")
+#elif n < 0:
+#    print ("the number is smaller 0")
+#else :
+#    print ("equal to 0")
+
+
+
+
+# sequences
+# name = ["harry", "baha" , "suhaib"]
+# n = int(input ("enter a number : "))
+# print(f" the {n} name is: {name[n]}")
+
+# DATA STRUCTURE
+# list      name = ["harry", "baha" , "suhaib"]
+# tuple 
+# set       name = set()
+# dict      name = {"harry":"amman", "baha":"turkey" , "suhaib":"istanbul"}
+
+# s = set ()
+# #for the set we can add by :
+# print (s)
+# s.add(0)
+# s.add(1)
+# s.add(2)
+# s.add(3)
+# s.add(4)
+# # and delete by:
+# s.remove(1)
+# # to print the length of any set:
+# print (f" the length for the set is: {len(s)}")
+
+
+# for loops
+# for i in (range(6)):
+#     print(i)
+# or
+# names="abcdefghijklmnopqrstu"
+# for name in names:
+#     print(name)
+
+
+# Dictionary 
+name = {"harry":"amman", "baha":"turkey" , "suhaib":"istanbul"}
+print(name["baha"])
