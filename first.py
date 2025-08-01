7c2f8fa2941bb900a1f4acd0af0879b15e15ee17
baha salameh <baha@MacBookAir.home>
2025-08-01 16:30:18 +0300

    adding some funtions

--- first.py
+++ first.py
@@ -1,4 +1,4 @@
-print ("hello, world!")
+# print ("hello, world!")
 
 # how to deal with str and value to print.
 #name = input("name: ")
@@ -55,10 +55,98 @@ print ("hello, world!")
 
 
 # Dictionary 
-name = {"harry":"amman", "baha":"turkey" , "suhaib":"istanbul"}
-print(name["baha"])
+# name = {"harry":"amman", "baha":"turkey" , "suhaib":"istanbul"}
+# name ["musab"]="istanbul"
+# print(name["musab"])
+
+
+# function
+
+# def square(x):
+#     return x * x
+
+# for i in range(10):
+#     print(f"the square of {i} is {square(i)}")
 
 
 
-#sdfsdfsdf
-baha=["sdfsd","sdfasd","sdfsdf"]
+# classes 
+# class point():
+#     def __init__(self, input1, input2):
+#         self.x = input1
+#         self.y = input2
+
+# p = point(2,4)
+# print(p.x)
+# print(p.y)
+
+####### Another example #####
+
+# class Flight():
+#     def __init__(self, capacity):
+#         self.capacity= capacity
+#         self.passengers=[]
+
+#     def add_passengers(self, name):
+#         if not self.remaining_seats():
+#             return False
+#         self.passengers.append(name)
+#         return True
+    
+#     def remaining_seats(self):
+#         return self.capacity - len(self.passengers)
+
+# flight = Flight(2)
+# people = ["baha", "baha", "baha"]
+# for name in people:
+#     if flight.add_passengers(name):
+#         print("done")
+#     else:
+#         print("no seats")
+
+
+
+
+
+
+########### DECORATORS ##############
+# def announce(f):
+#     def wrapper():
+#         print("about to run the function")
+#         f()
+#         print("done running the function")
+#     return wrapper 
+
+
+# @announce
+# def hello():
+#     print("hello world!")
+
+# hello()
+
+
+
+
+
+############### lambda ########################
+
+people = [{"name" : "c", "house": "b"},
+          {"name" : "b", "house": "a"},
+          {"name" : "a", "house": "c"},
+]
+
+def f(person):
+    return person ["name"]
+    
+people.sort(key=f)
+print(people)
+
+# better way to maintain the same result is by using lambda and there is how.
+
+people = [{"name" : "c", "house": "b"},
+          {"name" : "b", "house": "a"},
+          {"name" : "a", "house": "c"},
+]
+
+people.sort(key=lambda person : person ["name"])
+print(people)
